0673102d5e88d7220eba963e931531ba4be46843
SuslikV <vx2010@i.ua>
2018-08-28 15:26:32 +0200

    win-capture: Import GetDpiForWindow() function
    Load WINAPI function available since Win10 version 1607, Anniversary
    Update (for non-server versions), to support automatic window DPI
    detection. Useful for Windows scaled up displays capture.

--- plugins/win-capture/dc-capture.h
+++ plugins/win-capture/dc-capture.h
@@ -32,3 +32,34 @@ extern void dc_capture_free(struct dc_capture *capture);
 
 extern void dc_capture_capture(struct dc_capture *capture, HWND window);
 extern void dc_capture_render(struct dc_capture *capture, gs_effect_t *effect);
+
+typedef UINT(WINAPI *GETDPIFORWINDOW)(HWND hwnd);
+//
+// Get WINAPI function GetDpiForWindow(); since Windows 10, version 1607 (AU)
+//
+#define GET_WINAPI_DPIFORWINDOW_FUNC(func_name, library_name) \
+static UINT func_name(HWND hwnd) \
+{ \
+	HMODULE phModule = NULL; \
+	static GETDPIFORWINDOW FUNC = NULL; \
+	static UINT ret = 0; \
+	static bool initialized = false; \
+\
+	if (!initialized) { \
+		if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_PIN, \
+				TEXT(#library_name), &phModule)) \
+			FUNC = (GETDPIFORWINDOW)GetProcAddress(phModule, \
+					"GetDpiForWindow"); \
+\
+		initialized = true; \
+		} \
+\
+	if (FUNC) \
+		ret = FUNC(hwnd); \
+\
+	return ret; \
+}
+//
+// call to GetDpiForWindow() function
+//
+GET_WINAPI_DPIFORWINDOW_FUNC(GetDForWnd, user32)
